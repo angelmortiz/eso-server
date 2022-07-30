132f9d943aeec1173b1f1fda0bd7c3a05cee8f1b
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-07-29 21:02:37 -0400

    Changing htmls for ejs files

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const rootDir = require('./util/path');
-const express = require ('express');
-const exp = express();
-const bodyParser = require('body-parser');
-const nutritionData = require('./routes/nutrition');
-const activitiesData = require('./routes/activities');
+const express = require ('express'); //importing framework
+const path = require('path');  //tool to create addresses
+const rootDir = require('./util/path');// importing utility to create paths
+const bodyParser = require('body-parser'); //parser to read info from client-side
+const nutritionData = require('./routes/nutrition'); //routes for nutrition
+const activitiesData = require('./routes/activities'); //routes for activities
+const exp = express(); //initializing express framework
 
+exp.set('view engine', 'ejs'); //activates ejs templates to create dynamic htmls
 exp.use(bodyParser.urlencoded({extended: false})); //parses the body that comes from the client
-exp.use(express.static(path.join(rootDir, 'public')));
-
-exp.use('/nutrition', nutritionData.routes);
-exp.use('/activities', activitiesData.routes);
+exp.use(express.static(path.join(rootDir, 'public'))); //uploads public files (css) to client
+exp.use('/nutrition', nutritionData.routes); //executes routes for nutrition
+exp.use('/activities', activitiesData.routes); //executes routes for activities
 
 //Implement 404 logic
 // exp.use('/*', (request, response, next) => {
--- routes/nutrition.js
+++ routes/nutrition.js
@@ -6,20 +6,23 @@ const router = express.Router();
 const foods = [];
 
 router.get('/food', (request, response, next) => {
-    response.sendFile(path.join(rootDir, 'views', 'nutrition', 'view-food.html'));
+  response.render('./nutrition/view-food', {
+    foods: foods,
+    pageTitle: 'Food Information'
+  });
 });
 
 router.get('/add-food', (request, response, next) => {
-    response.sendFile(path.join(rootDir, 'views', 'nutrition', 'add-food.html'));
+  response.render('./nutrition/add-food');
 });
 
 router.post('/food', (request, response, next) => {
-    //add food info to db
+  //add food info to db
 
-    foods.push({name: request.body.name})
-    console.log(foods);
-    // response.redirect('/nutrition/food')
+  foods.push({ name: request.body.name });
+  console.log(foods);
+  // response.redirect('/nutrition/food')
 });
 
 exports.routes = router;
-exports.foods = foods; 
+exports.foods = foods;
