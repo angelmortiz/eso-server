b57e3a06322beaa1a310b9640fb78b95f4259304
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-08-01 14:01:50 -0400

    Adding muscle pages

--- routes/activities.js
+++ routes/activities.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const exercises = [];
+const muscles = [];
 
 /* EXERCISE */
 router.get('/exercise', (request, response, next) => {
@@ -25,6 +26,21 @@ router.post('/exercise', (request, response, next) => {
     console.log(exercises);
 
     // response.redirect('/activities/exercise');
+});
+
+/* MUSCLE */
+router.get('/muscle', (request, response, next) => {
+    response.render('./activities/view-muscle', {
+        muscles: muscles,
+        pageTitle: 'Información de músculo'
+      });
+});
+
+router.get('/add-muscle', (request, response, next) => {
+    response.render('./activities/add-muscle', {
+        muscles: muscles,
+        pageTitle: 'Añadir músculo'
+      });
 });
 
 exports.routes = router;
