68943cf5ecf579aeaa282ea95830b75bbec93cb4
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-08-05 12:45:28 -0400

    renaming routers

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require ('express'); //importing framework
 const path = require('path');  //tool to create addresses
 const rootDir = require('./util/path');// importing utility to create paths
 const bodyParser = require('body-parser'); //parser to read info from client-side
-const nutritionRoutes = require('./routes/nutrition'); //routes for nutrition
-const activitiesRoutes = require('./routes/activities'); //routes for activities
+const nutritionRouter = require('./routes/nutritionRouter'); //routes for nutrition
+const activitiesRouter = require('./routes/activitiesRouter'); //routes for activities
 const homeController = require('./controllers/homeController'); //imports logic to load home page
 const errorController = require('./controllers/errorsController'); //imports logic to load home page
 const exp = express(); //initializing express framework
@@ -15,8 +15,8 @@ exp.use(bodyParser.urlencoded({extended: false})); //parses the body that comes 
 exp.use(express.static(path.join(rootDir, 'public'))); //uploads public files (css) to client
 
 exp.get('/', homeController.getHome); //navigates to home if no other address was provided
-exp.use('/nutrition', nutritionRoutes.routes); //executes routes for nutrition
-exp.use('/activities', activitiesRoutes.routes); //executes routes for activities
+exp.use('/nutrition', nutritionRouter.routes); //executes routes for nutrition
+exp.use('/activities', activitiesRouter.routes); //executes routes for activities
 exp.use('/', errorController.get404); //navigates to 404 error if the address provided does not exist
 
 //connects to the database and starts the server after it's connected
