b106ce15d1df46b852bc0a070c5f50cfe8622a1c
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-08-08 21:34:57 -0400

    Adding logic for notrecommended for conditions

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,58 @@
-//GLOBAL
+//***GLOBAL
 const SERVER_ADDRESS = "http://localhost:3000/api";
-
-/** SAFE FOR CONDITIONS **/
-//elements
-const btnSafeForConditions = document.querySelector('#btn-add-new-safe-condition');
-const safeForCondtionsDiv = document.querySelector('#safeForConditions-selects');
-
-//variables
 let chronicConditions;
-
-async function addNewSafeCondition() {
-    if (!chronicConditions) { await getChronicConditions(); }
-    const selectNode = document.createElement("select");
-    safeForCondtionsDiv.appendChild(selectNode);
-
-    console.log('chronicConditions 1', chronicConditions);
-
-    chronicConditions.forEach(cc => {
-        let option = new Option(cc.name, cc._id);
-        selectNode.appendChild(option);
-    });
-};
 
 //FIXME: Implement a better async solution to fetch values
 let getChronicConditions = async () => {
     const response = await fetch(`${SERVER_ADDRESS}/nutrition/chronicConditions`);
     chronicConditions = await response.json();
 };
+//*** [END] GLOBAL
+
+/** SAFE FOR CONDITIONS **/
+//elements
+const btnSafeForConditions = document.querySelector('#btn-add-new-safe-condition');
+const safeForCondtionsDiv = document.querySelector('#safeForConditions-selects');
+
+async function addNewSafeCondition() {
+    //pulls conditions names and ids from server
+    if (!chronicConditions) { await getChronicConditions(); }
+
+    //creates new dropdown element and adds it to the DOM
+    const selectNode = document.createElement("select");
+    safeForCondtionsDiv.appendChild(selectNode);
+
+    //adds options to the dropdown
+    chronicConditions.forEach(cc => {
+        let option = new Option(cc.name, cc._id);
+        selectNode.appendChild(option);
+    });
+};
 
 //listeners
 btnSafeForConditions?.addEventListener('click', addNewSafeCondition);
 /** [END] SAFE FOR CONDITIONS **/
+
+/** NOT RECOMMENDED FOR CONDITIONS **/
+//elements
+const btnNotRecommendedForConditions = document.querySelector('#btn-add-new-not-recommended-condition');
+const notRecommendedForCondtionsDiv = document.querySelector('#notRecommendedForConditions-selects');
+
+async function addNewNotRecommendedForCondition() {
+    //pulls conditions names and ids from server
+    if (!chronicConditions) { await getChronicConditions(); }
+
+    //creates new dropdown element and adds it to the DOM
+    const selectNode = document.createElement("select");
+    notRecommendedForCondtionsDiv.appendChild(selectNode);
+
+    //adds options to the dropdown
+    chronicConditions.forEach(cc => {
+        let option = new Option(cc.name, cc._id);
+        selectNode.appendChild(option);
+    });
+};
+
+//listeners
+btnNotRecommendedForConditions?.addEventListener('click', addNewNotRecommendedForCondition);
+/** [END] NOT RECOMMENDED CONDITIONS **/
