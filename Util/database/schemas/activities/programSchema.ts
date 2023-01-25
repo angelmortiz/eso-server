b794b7e8f419831b506bbb7121aa9f1bff151179
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-01-25 15:49:41 -0400

    creating schemas

---
+++ Util/database/schemas/activities/programSchema.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const WorkoutPlanSchema = new Schema({
+  workoutId: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'Workout id is required.'],
+  },
+  dayNumber: {
+    type: Number,
+    required: false,
+  },
+  dayOfTheWeek: {
+    type: String,
+    enum: [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thrusday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ],
+    required: false,
+  },
+});
+
+export default new Schema({
+  name: {
+    type: String,
+    required: [true, 'Program name is required.'],
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  type: {
+    type: String,
+    enum: ['Strength', 'Hypertrophy', 'Endurance', 'Mixed'],
+    required: [true, 'Program type is required.'],
+  },
+  sequence: {
+    type: String,
+    enum: ['Weekly', 'Cycle'],
+    required: [
+      true,
+      'Program sequence is required. Please, choose between weekly or cycle.',
+    ],
+  },
+  duration: {
+    type: String,
+    required: false
+  },
+  workouts: {
+    type: [WorkoutPlanSchema],
+    required: false
+  }
+});
